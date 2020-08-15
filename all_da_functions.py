7cbc1062070a8cee28c50653b4e4a0f4ee137d7e
Zax71 <67716263+zax71@users.noreply.github.com>
2020-08-15 17:56:15 +0100

    made it neater

--- all_da_functions.py
+++ all_da_functions.py
@@ -25,33 +25,33 @@ mbholddown_time = 0 #the time that the mouse button whill be held down for (in s
 text_to_spam = "" #the text to spam                                                                        ¦
 #                                                                                                          ¦
 ammount_to_spam = 0 #the ammount of time to spam "text_to_spam" veriable                                   ¦
-#clicker functions-----------------------------------------------------------------------------------------
-def clickerstart():  #this functions activates when you press the "start" button in the autocklicker menu  ¦
-
-    if clickerlmb == 1: #if you sellected to click the LMB then this block of code whill be run
-
-        for i in range(0, clickertime): #looping for "clickertime" so we get more than 1 second of clicking
-
-            for i in range(0, CPS): #looping for the CPS so we get more than 1 click
-
-                delay = 1 / CPS #working out the delay from the CPS value
-                mouse.press(Button.left) #pressing the LMB...
-                print("click")
-                time.sleep(int(delay)) #sleeping for the before worked out time
-                mouse.release(Button.left) #...and releasing it
-
-    
-    if clickerrmb == 1: #if you sellected to click the LMB then this block of code whill be run
-
-        for i in range(0, clickertime): #looping for "clickertime" so we get more than 1 second of clicking
-
-            for i in range(0, CPS): #looping for the CPS so we get more than 1 click
-
-                delay = 1 / CPS #working out the delay from the CPS value
-                mouse.press(Button.right) #pressing the RMB...
-                print("click")
-                time.sleep(int(delay)) #sleeping for the before worked out time
-                mouse.release(Button.right) #...and releasing it
+#clicker functions---------------------------------------------------------------------------------------------
+def clickerstart():  #this functions activates when you press the "start" button in the autocklicker menu      ¦
+#                                                                                                              ¦
+    if clickerlmb == 1: #if you sellected to click the LMB then this block of code whill be run                ¦
+#                                                                                                              ¦
+        for i in range(0, clickertime): #looping for "clickertime" so we get more than 1 second of clicking    ¦
+#                                                                                                              ¦
+            for i in range(0, CPS): #looping for the CPS so we get more than 1 click                           ¦
+#                                                                                                              ¦
+                delay = 1 / CPS #working out the delay from the CPS value                                      ¦
+                mouse.press(Button.left) #pressing the LMB...                                                  ¦
+#                                                                                                              ¦
+                time.sleep(int(delay)) #sleeping for the before worked out time                                ¦
+                mouse.release(Button.left) #...and releasing it                                                ¦
+#                                                                                                              ¦
+#                                                                                                              ¦
+    if clickerrmb == 1: #if you sellected to click the LMB then this block of code whill be run                ¦
+#                                                                                                              ¦
+        for i in range(0, clickertime): #looping for "clickertime" so we get more than 1 second of clicking    ¦
+#                                                                                                              ¦
+            for i in range(0, CPS): #looping for the CPS so we get more than 1 click                           ¦
+#                                                                                                              ¦
+                delay = 1 / CPS #working out the delay from the CPS value                                      ¦
+                mouse.press(Button.right) #pressing the RMB...                                                 ¦
+#                                                                                                              ¦
+                time.sleep(int(delay)) #sleeping for the before worked out time                                ¦
+                mouse.release(Button.right) #...and releasing it                                               ¦
 #mouse button functions ---------------------------------------------------------------------------------------
 def mbholddown_start(): #when you press start on the MB hold down program, this run's                          ¦
 #                                                                                                              ¦ 
@@ -64,7 +64,7 @@ def mbholddown_start(): #when you press start on the MB hold down program, this 
         mouse.press(Button.left) #press RMB...                                                                 ¦
         time.sleep(mbholddown_time) #sleep for the time specified in the GUI                                   ¦
         mouse.release(Button.left) #...and release it                                                          ¦
-#text spammer functions------------------------------------------------------------------------------------
+#text spammer functions----------------------------------------------------------------------------------------
 def text_spam_start(): #this function whill run when you click start on the text spamer                    ¦
     for i in range(0, ammount_to_spam): #loop for the ammount of stuff to spam                             ¦
         keyboard.type(text_to_spam) #typing the spam text                                                  ¦
